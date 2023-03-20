0bd8efe095d017ca2c14e6d9f41cf9916eb04647
nihalsj12 <nihalsajjaa12@gmail.com>
2023-03-20 07:58:39 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,15 @@
+'''module'''
+
+
+def funct(x_var1):
+    '''fonction 1'''
+    return x_var1 + 1
+
+def function_1(a_var,b_var):
+    '''fonction 2'''
+    return a_var + b_var
+
+def function_2(z_var,y_var):
+    '''fonction 3'''
+    return z_var - y_var
+    
---
+++ test_py.py
@@ -0,0 +1,13 @@
+'''import app'''
+import app 
+
+
+def test_fonc():
+    '''test 1'''
+    assert app.funct(1) == 2
+def test_fun1():
+    '''test 2'''
+    assert app.function_1(2,3) == 5
+def test_fun2():
+    '''test 3'''
+    assert app.function_2(5,2) == 3
